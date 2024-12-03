e4055106d8198123301f6123f223128a4abcbecf
Christianjcope <christian1jcope@gmail.com>
2024-12-03 11:12:49 -0500

    Added basic Javascript for Registration

---
+++ main.js
@@ -0,0 +1,13 @@
+function submitStuff(){
+    const fname = document.getElementById("firstName").value
+    const lname = document.getElementById("lastName").value
+    const email = document.getElementById("Email").value
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+
+    console.log(fname)
+    console.log(lname)
+    console.log(email)
+    console.log(username)
+    console.log(password)
+}
