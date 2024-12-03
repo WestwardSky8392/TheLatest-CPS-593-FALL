621f499a67f807573e9c00bb5bce61a07ad47684
Christianjcope <christian1jcope@gmail.com>
2024-12-03 11:26:06 -0500

    Added more Javascript for Login

---
+++ Login.js
@@ -0,0 +1,7 @@
+function submitStuff2(){
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+
+    console.log(username)
+    console.log(password)
+}
