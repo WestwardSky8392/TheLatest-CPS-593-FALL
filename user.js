5e7b2e0f1079b2d91c5db1c9e0d641556c9f123e
Christianjcope <christian1jcope@gmail.com>
2024-12-04 21:44:43 -0500

    Stuff

--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-let form2 = document.getElementById("form2")
+let form2 = document.getElementById("form1")
 form2.addEventListener('submit', submitStuff2);
 
 function submitStuff2(e){
@@ -7,5 +7,9 @@ function submitStuff2(e){
         username: document.getElementById("username").value,
         password: document.getElementById("password").value
         }
+
         console.log(USER)
+        //some form of statement will check database is user exists with their password
+        // and let them proceed to the post/media aspect of website.
+        window.location.href = ('post.html')
     }
