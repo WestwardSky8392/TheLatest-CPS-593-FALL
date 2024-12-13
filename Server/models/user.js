2ef5a5fe5f7ab5e2dc92bac18858e7d32c94b085
Christianjcope <christian1jcope@gmail.com>
2024-12-13 17:51:33 -0500

    FIxed Users

--- Server/models/user.js
+++ Server/models/user.js
@@ -2,16 +2,25 @@ const users = [
   {
     user_id : 15432,
     username : "christian2",
-    passowrd : "icecream2" 
+    passowrd : "icecream2", 
+    fullName: "Christian Copeland",
+    phoneNum: 8452760060,
+    email: "chriscope@gmail.com"
   },
   {
     user_id : 77777,
     username : "rock47",
-    passowrd : "93.3" 
+    passowrd : "93.3" ,
+    fullName: "Dwane Johnson",
+    phoneNum: 6764449090,
+    email: "THEROCK@gmail.com"
   },
   {
     user_id : 15432,
     username : "kevinMitch",
-    passowrd : "ilik3kats" 
+    passowrd : "ilik3kats", 
+    fullName: "Kevin Mitchell;",
+    phoneNum: 5555555555,
+    email: "KmITch@yahoo.com"
   },
 ]
